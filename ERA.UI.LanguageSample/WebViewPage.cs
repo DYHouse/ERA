035b1fedd010099b068e5a50db085f1ada84791f
AaronPan <panyuyu2014@163.com>
2014-10-31 09:56:28 +0800

    update Language

---
+++ ERA.UI.LanguageSample/WebViewPage.cs
@@ -0,0 +1,31 @@
+﻿using ERA.Framework.Cache;
+using ERA.Framework.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace ERA.UI.LanguageSample
+{
+    public abstract class WebViewPage<TModel> : System.Web.Mvc.WebViewPage<TModel>
+    {
+        public  Localizer T
+        {
+            get
+            {
+                return new LocalizedManager(new DefaultLanguageResourceProvider(),
+                    new MemoryCacheManager(),
+                    Thread.CurrentThread.CurrentCulture.Name).T;
+            }
+        }
+
+        public override void InitHelpers()
+        {
+            base.InitHelpers();
+        }
+    }
+    public abstract class WebViewPage : WebViewPage<dynamic>
+    {
+    }
+}
