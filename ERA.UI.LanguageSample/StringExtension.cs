cd8dea95f4b632ada83f9e33066e00d121796eda
AaronPan <panyuyu2014@163.com>
2014-10-31 09:29:42 +0800

    update language

---
+++ ERA.UI.LanguageSample/StringExtension.cs
@@ -0,0 +1,20 @@
+﻿using ERA.Framework.Cache;
+using ERA.Framework.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace ERA.UI.LanguageSample
+{
+    public static class StringExtension
+    {
+        public static string ToLocalString(this string str, params string[] args)
+        {
+            return new LocalizedManager(new DefaultLanguageResourceProvider()
+                , new MemoryCacheManager()
+                , Thread.CurrentThread.CurrentCulture.Name).T(str, args).ToString();
+        }
+    }
+}
