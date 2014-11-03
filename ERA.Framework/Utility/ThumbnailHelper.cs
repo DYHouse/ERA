6ae6eb92038a810b50bb54cfd90234e54606994c
AaronPan <panyuyu2014@163.com>
2014-11-03 10:19:20 +0800

    update

---
+++ ERA.Framework/Utility/ThumbnailHelper.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERA.Framework.Utility
+{
+    public class ThumbnailHelper
+    {
+        /// <summary>
+        /// 创建缩略图
+        /// </summary>
+        public static void CutAvatar(string imgSrc, int x, int y, int width, int height, long Quality, string SavePath, int t_width,int t_height)
+        {
+            Image original = Image.FromFile(imgSrc);
+            Bitmap img = new Bitmap(t_width, t_height, PixelFormat.Format24bppRgb);
+            img.MakeTransparent(img.GetPixel(0, 0));
+            img.SetResolution(72, 72);
+            using (Graphics gr = Graphics.FromImage(img))
+            {
+                if (original.RawFormat.Equals(ImageFormat.Jpeg) || original.RawFormat.Equals(ImageFormat.Png) || original.RawFormat.Equals(ImageFormat.Bmp))
+                {
+                    gr.Clear(Color.Transparent);
+                }
+                if (original.RawFormat.Equals(ImageFormat.Gif))
+                {
+                    gr.Clear(Color.White);
+                }
+                gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                gr.SmoothingMode = SmoothingMode.AntiAlias;
+                gr.CompositingQuality = CompositingQuality.HighQuality;
+                gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                gr.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+                using (var attribute = new System.Drawing.Imaging.ImageAttributes())
+                {
+                    attribute.SetWrapMode(WrapMode.TileFlipXY);
+                    gr.DrawImage(original, new Rectangle(0, 0, t_width, t_height), x, y, width, height, GraphicsUnit.Pixel, attribute);
+                }
+            }
+            ImageCodecInfo myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            if (original.RawFormat.Equals(ImageFormat.Jpeg))
+            {
+                myImageCodecInfo = GetEncoderInfo("image/jpeg");
+            }
+            else
+                if (original.RawFormat.Equals(ImageFormat.Png))
+                {
+                    myImageCodecInfo = GetEncoderInfo("image/png");
+                }
+                else
+                    if (original.RawFormat.Equals(ImageFormat.Gif))
+                    {
+                        myImageCodecInfo = GetEncoderInfo("image/gif");
+                    }
+                    else
+                        if (original.RawFormat.Equals(ImageFormat.Bmp))
+                        {
+                            myImageCodecInfo = GetEncoderInfo("image/bmp");
+                        }
+
+            System.Drawing.Imaging.Encoder myEncoder = System.Drawing.Imaging.Encoder.Quality;
+            EncoderParameters myEncoderParameters = new EncoderParameters(1);
+            EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, Quality);
+            myEncoderParameters.Param[0] = myEncoderParameter;
+            img.Save(SavePath, myImageCodecInfo, myEncoderParameters);
+        }
+
+        private static ImageCodecInfo GetEncoderInfo(String mimeType)
+        {
+            int j;
+            ImageCodecInfo[] encoders;
+            encoders = ImageCodecInfo.GetImageEncoders();
+            for (j = 0; j < encoders.Length; ++j)
+            {
+                if (encoders[j].MimeType == mimeType)
+                    return encoders[j];
+            }
+            return null;
+        }
+    }
+}
